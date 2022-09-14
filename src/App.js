b672e481ec4ded36ed9606714d5fdd1c3a801aff
Piyush <bhangalepiyush@gmail.com>
2022-09-14 21:37:34 +0530

    feat: persist user authentication on refresh

--- src/App.js
+++ src/App.js
@@ -1,45 +1,52 @@
 import './App.css';
 import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import CreateBlog from './screens/CreateBlog';
 import Login from './screens/Login';
 import Home from './screens/Home';
 import { signOut } from 'firebase/auth'
 import { auth } from './firebaseConfig';
+import { useEffect } from 'react';
 
 function App() {
 
-  const [authenticated, setAuthenticated] = useState(false) 
+  const [authenticated, setAuthenticated] = useState(false)
 
   const logout = () => {
-      signOut(auth).then(() => {
-        localStorage.clear();
-        setAuthenticated(false)
-        window.location.pathname = "/login"  //don't use navigate here // we are outside of router lol
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setAuthenticated(false)
+      window.location.pathname = "/login"  //don't use navigate here // we are outside of router lol
 
-      })
+    })
   }
 
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      setAuthenticated(user ? true : false)
+    })
+  }, [])
+
   return (
-     <Router>
+    <Router>
       <nav>
-        
-        
+
+
         {!authenticated
-         ? <Link to="/login">Login</Link> 
-        :<>
-        <Link to="/">Home</Link>
-        <Link to="/CreateBlog">CreateBlog</Link> 
-        <button onClick={logout} className='logout-btn'>LOGOUT</button></> }  
+          ? <Link to="/login">Login</Link>
+          : <>
+            <Link to="/">Home</Link>
+            <Link to="/CreateBlog">CreateBlog</Link>
+            <button onClick={logout} className='logout-btn'>LOGOUT</button></>}
       </nav>
       <Routes>
         <Route path='/' element={<Home authenticated={authenticated} />}></Route>
         <Route path='/CreateBlog' element={<CreateBlog authenticated={authenticated} />}></Route>
-       <Route path='/login' element={<Login setAuthenticated={setAuthenticated} />}></Route>
-        
+        <Route path='/login' element={<Login setAuthenticated={setAuthenticated} />}></Route>
+
       </Routes>
-     </Router>
+    </Router>
   );
 }
 
