50eff075b3ee7bc037d5a66224fb81fc0671eee8
Piyush <bhangalepiyush@gmail.com>
2022-09-14 21:35:15 +0530

    refactor: seperate blog posts, use Blog component

---
+++ src/components/Blog.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Blog({ blog }) {
+    const [edit, setEdit] = useState(false);
+    const [newtitle, setNewTitle] = useState("")
+
+    return (
+        <div className='card' key={blog.id}>
+            <p className='title'>{blog.title}<button onClick={() => setEdit(true)}>edit</button> </p>
+            {
+                edit ?
+                    <>
+                        <input
+                            type="text"
+                            placeholder='New Blog Title...'
+                            value={newtitle}
+                            onChange={(e) => setNewTitle(e.target.value)}>
+                        </input>
+                        <button>Save</button>
+                    </>
+                    : null
+            }
+            <p className='content'>{blog.content}</p>
+            {blog.author ? <p className='content'><b>@{blog.author.name}</b></p> : <p className='content'>unknown user</p>}
+        </div>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,50 +1,45 @@
+import { collection, getDocs, query } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { collection,getDocs,query } from 'firebase/firestore'
-import { db, auth } from '../firebaseConfig'
 import { useNavigate } from 'react-router-dom'
+import Blog from '../components/Blog'
+import { db } from '../firebaseConfig'
 
 const Home = ({ authenticated }) => {
   const navigate = useNavigate()
-  const [blogs,setBlogs] = useState([]);
-  const [loading,setLoading] = useState(true);
-  const [edit,setEdit] = useState(false);
-  const [newtitle,setNewTitle] = useState("")
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
 
-    if(!authenticated){
+    if (!authenticated) {
       navigate("/login")
-     }
+    }
 
     const getAllBlogs = async () => {
 
-      const q = query(collection(db,"blogs"))
-  
+      const q = query(collection(db, "blogs"))
+
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-       setBlogs(querySnapshot.docs.map((doc) => ({...doc.data(),id:doc.id})))
-       setLoading(false)
-    })
+        setBlogs(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        setLoading(false)
+      })
 
     }
 
     getAllBlogs()
-  },[])
+  }, [])
+
   return (<>
-    {loading ? <div className="lds-ring"><div></div><div></div><div></div><div></div></div> :  <div className='card-div'>
-    
-    {blogs.map((blog) => {
-      console.log(blog.author);
-      return <div className='card' key={blog.id}>
-     <p className='title'>{blog.title}<button onClick={() => setEdit(true)}>edit</button> </p>
-     { edit ? <><input type="text" placeholder='New Blog Title...' value={newtitle} onChange={(e) => setNewTitle(e.target.value)}></input><button>Save</button></> : null}
-     <p className='content'>{blog.content}</p>
-     {blog.author ? <p className='content'><b>@{blog.author.name}</b></p> : <p className='content'>unknown user</p>}
-    </div>
- })}
-   </div> }
-    
-   </>)
+    {loading ? <div className="lds-ring"><div></div><div></div><div></div><div></div></div> : <div className='card-div'>
+
+      {blogs.map((blog) => {
+        console.log(blog.author);
+        return <Blog blog={blog} />
+      })}
+    </div>}
+
+  </>)
 }
 
 export default Home
